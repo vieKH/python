4aaa96fe7e4f459d19cca14d28ea80daf244ae20
vieKH <114916103+vieKH@users.noreply.github.com>
2022-11-07 18:07:04 +0400

    Update copy_dataset.py
    Исправил lowcase  и когда было except завершить программу

--- Lab2/copy_dataset.py
+++ Lab2/copy_dataset.py
@@ -13,8 +13,9 @@ def copy_dataset(path: str, path_copy: str, ann: Annotation) -> None:
     if not os.path.isdir(path_copy):
         try:
             os.mkdir(path_copy)
-        except OSError:
+        except OSError as err:
             print(f"Создать директору {path_copy} не успешно")
+            raise err
     folders = os.listdir(path)
     for folder in folders:
         files = os.listdir(os.path.join(path, folder))
@@ -27,5 +28,5 @@ def copy_dataset(path: str, path_copy: str, ann: Annotation) -> None:
 if __name__ == "__main__":
     path_copy = 'C:/Users/Admin/Desktop/Study/Python/lab 2/dataset_copy'
     path = 'C:/Users/Admin/Desktop/Study/Python/pythonProject/dataset'
-    A = Annotation("file_csv_copy.csv")
-    copy_dataset(path, path_copy, A)
+    a = Annotation("file_csv_copy.csv")
+    copy_dataset(path, path_copy, a)
