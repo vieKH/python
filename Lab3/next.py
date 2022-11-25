f4a86195a55af8901a70c4ccb4e5e63d4b52d64f
KH <10122001kh@mail.ru>
2022-11-25 21:43:01 +0400

    Add file

---
+++ Lab3/next.py
@@ -0,0 +1,18 @@
+import os
+import re
+
+
+def next_file(path: str):
+    """
+    :param path: Путь к папки
+    :return: Следующий экземпляр класса или None
+    """
+    if not os.path.exists(path):
+        raise FileExistsError(f'Файл по {path} не существует')
+    direct, filename = os.path.split(path)
+    a = "".join(re.findall(r'\d', filename))
+    number = int(a) + 1
+    file_new = re.sub(a, f'{number:04d}', filename)
+    file_new = os.path.join(direct, file_new)
+    if os.path.exists(file_new):
+        return file_new
